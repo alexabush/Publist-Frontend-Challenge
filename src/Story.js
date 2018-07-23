c16fe7d26ef6fdfdb1788fd19f88bd3407bafcd2
Alex Bush <alexabush7@gmail.com>
2018-07-23 12:22:26 -0700

    working on adding styling to Story component

--- src/Story.js
+++ src/Story.js
@@ -8,21 +8,21 @@ class Story extends Component {
       url,
       title,
       description,
-      score,
-      read_title
+      read_time
     } = this.props.storyData;
+    const score = Math.round(this.props.storyData.score);
     return (
       <div className="Story">
         <div className="Image__container">
           <img src={image_url} alt="" />
         </div>
-        <div>
+        <div className="Story__content">
           <h2>{title}</h2>
-          <p>{description}</p>
+          {/* <p className="description">{description}</p> */}
           <div className="Story__div--row">
-            <p>{score}</p>
+            <p>Score: {score}</p>
             <p>{this.props.storyData.site.name}</p>
-            <p>{read_title}</p>
+            <p>Read Time: {read_time}s</p>
           </div>
         </div>
       </div>

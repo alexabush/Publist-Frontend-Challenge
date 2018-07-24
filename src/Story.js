79bb11a9096174222e3839c37293fca5028a8e1a
Alex Bush <alexabush7@gmail.com>
2018-07-24 10:54:34 -0700

    cleaned up code

--- src/Story.js
+++ src/Story.js
@@ -2,14 +2,12 @@ import React, { Component } from 'react';
 import './Story.css';
 
 class Story extends Component {
-  state = {};
-
   toUrl = () => {
     window.location = this.props.storyData.url;
   };
 
   render() {
-    const { image_url, title, description, read_time } = this.props.storyData;
+    const { image_url, title, read_time } = this.props.storyData;
     const score = Math.round(this.props.storyData.score);
     return (
       <div className="Story" onClick={this.toUrl}>
